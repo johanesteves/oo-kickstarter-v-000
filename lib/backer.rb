5169ba1ce69c8a6dcfab7fd58b0218f20cf35884
Johan Esteves <jsesteves91@gmail.com>
2017-09-11 01:11:03 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,6 +5,9 @@ attr_accessor :name, :backed_projects
   def initialize(name)
     @name = name
     @backed_projects = []
+  end
 
+  def back_project(project)
+    self.backed_projects <<  project
   end
 end
