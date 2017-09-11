a215cf22669694787e2f6058c5abb67da4f3149b
Johan Esteves <jsesteves91@gmail.com>
2017-09-11 01:19:27 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,15 +8,11 @@ attr_accessor :name, :backed_projects
   end
 
   def back_project(project)
-    back_project(project)
     self.backed_projects <<  project
-
-  end
-
-  def back_project(project)
     project.backers << self unless project.backers.include?(project)
 
   end
+
 
 
 
